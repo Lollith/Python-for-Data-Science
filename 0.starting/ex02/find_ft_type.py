f69cb06d360cea0813064fc8c63242332425c301
lollith <adeline.gouet@gmail.com>
2023-10-26 18:21:40 +0200

    indent ok

--- 0.starting/ex02/find_ft_type.py
+++ 0.starting/ex02/find_ft_type.py
@@ -1,17 +1,7 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    find_ft_type.py                                    :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: agouet <agouet@student.42.fr>              +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2023/10/26 13:21:36 by agouet            #+#    #+#              #
-#    Updated: 2023/10/26 18:06:40 by agouet           ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
+""" Module that contains a function that takes any object as argument and define its type."""
 
 def all_thing_is_obj(input_object: any) -> int:
-	"""Function that takes any object as argument and define its type.
+    """Function that takes any object as argument and define its type.
 
 	parameters
 	----------
@@ -22,22 +12,21 @@ def all_thing_is_obj(input_object: any) -> int:
 	-------
 	int
 		1 if the object is a container, 0 otherwise """
-
-	if isinstance(input_object, list):
-		print("List: ", type(input_object))
-		return 1
-	if isinstance(input_object, tuple):
-		print("Tuple: ", type(input_object))
-		return 1
-	if isinstance(input_object, set):
-		print("Set: ", type(input_object))
-		return 1
-	if isinstance(input_object, dict):
-		print("Dict: ", type(input_object))
-		return 1
-	if isinstance(input_object, str):
-		print(input_object, "is in the kitchen", type(input_object))
-		return 1
-	else:
-		print("Type not found")
-		return 42
+    if isinstance(input_object, list):
+        print("List: ", type(input_object))
+        return 1
+    if isinstance(input_object, tuple):
+        print("Tuple: ", type(input_object))
+        return 1
+    if isinstance(input_object, set):
+        print("Set: ", type(input_object))
+        return 1
+    if isinstance(input_object, dict):
+        print("Dict: ", type(input_object))
+        return 1
+    if isinstance(input_object, str):
+        print(input_object, "is in the kitchen", type(input_object))
+        return 1
+    else:
+        print("Type not found")
+        return 42
