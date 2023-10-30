0d913be8c25de18f1efe0fbd68b46b95442c0cea
lollith <adeline.gouet@gmail.com>
2023-10-30 17:36:24 +0100

    ...

--- 0.starting/ex03/NULL_not_found.py
+++ 0.starting/ex03/NULL_not_found.py
@@ -12,14 +12,12 @@ def NULL_not_found(input_object: any) -> int:
         bool: "Fake",
     }
     type_object = type(input_object)
-    object_zero = my_objects_zero.get(type_object)
-    if type_object == str:
+    if (input_object and type_object != float):
         print("Type not found")
-    elif object_zero is None:
-        print(f"Nothing: {input_object} {type_object}")
     else:
-        print(f"{object_zero}: {input_object} {type_object}")
+        object_zero = my_objects_zero.get(type_object)
+        if object_zero is None:
+            print(f"Nothing: {input_object} {type_object}")
+        else:
+            print(f"{object_zero}: {input_object} {type_object}")
     return 1
-
-
-en cours
---
+++ 0.starting/ex04/whatis.py
@@ -0,0 +1,17 @@
+"""Script that takes a number as argument, checks whether it is odd or even,
+and prints the result."""
+
+import sys
+
+if len(sys.argv) < 1:
+	sys.exit(1)
+try:	
+	if len(sys.argv) > 1:
+		raise AssertionError("More than one argument is provided")
+except AssertionError as error:
+	print("error")
+	sys.exit(1)
+
+# try:
+# 	sys.exit(0)
+# assert len(sys.argv) > 1, "ERROR"
