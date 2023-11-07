ad1c31528e7d02e156742ecd1e26c8eab29e7a7a
lollith <adeline.gouet@gmail.com>
2023-11-07 17:51:25 +0100

    ok

--- 1-Array/ex00/give_bmi.py
+++ 1-Array/ex00/give_bmi.py
@@ -3,12 +3,24 @@ def give_bmi(height: list[int | float], weight: list[int | float]) \
     """Take 2 lists of intergers or floats in input and returns a list of BMI
     values."""
     try:
-        for i in height:
-            if not isinstance(i, float) or isinstance(i, int):
-                raise TypeError("not int/float")
+        try:
+            for h, w in zip(height, weight):
+                if not isinstance(h, (float, int)) \
+                        or not isinstance(w, (int, float)):
+                    raise TypeError()
+
+            for h in height:
+                if h == 0:
+                    raise ZeroDivisionError()
+
+        except TypeError:
+            raise AssertionError("not int/float")
+        except ZeroDivisionError:
+            raise AssertionError("wrong value")
 
         if (len(height) != len(weight)):
             raise AssertionError("Not same Size")
+
     except AssertionError as error:
         print(AssertionError.__name__, ":", error)
     else:
@@ -41,7 +53,33 @@ if __name__ == "__main__":
     print(apply_limit(bmi, 26))
 
     print()
-    print("Not int/float")
+    print("Not int/float 1")
     height = ["ade", "coucou"]
-    weight = [1.2, 2.2]
+    weight = [2, 2.2]
     bmi = give_bmi(height, weight)
+
+    print()
+    print("Not int/float 2")
+    height = [1.3, 1.4]
+    weight = [1.3, "ade"]
+    bmi = give_bmi(height, weight)
+
+    print()
+    print("Not int/float 3")
+    height = ["ade", 1.4]
+    weight = [1.3, "ade"]
+    bmi = give_bmi(height, weight)
+
+    print()
+    print("value 0")
+    height = [0, 1.4]
+    weight = [1.3, 2.2]
+    bmi = give_bmi(height, weight)
+    print()
+    
+    print("value -1")
+    height = [2.71, 1.15]
+    weight = [-165, 38.4]
+    bmi = give_bmi(height, weight)
+    print(bmi, type(bmi))
+    print(apply_limit(bmi, 26))
---
+++ 1-Array/ex01/array2D.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+def slice_me(family: list, start: int, end: int) -> list:
+    """function that takes as parameters a 2D array, prints its shape, and
+    returns a truncated version of the array based on the provided start and
+    end arguments."""
