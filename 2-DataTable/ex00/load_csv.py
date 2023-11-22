62b3fad0a08eff07dc82106cab22dba8eed34cc9
lollith <adeline.gouet@gmail.com>
2023-11-22 18:08:05 +0100

    ok

--- 2-DataTable/ex00/load_csv.py
+++ 2-DataTable/ex00/load_csv.py
@@ -1,10 +1,31 @@
 import pandas as pd
-from load_csv import load
 
-def load(path: str) -> Dataset:
+def load(path: str) -> pd.DataFrame :
     '''takes a path as argument, writes the dimensions of the data set
 and returns it.'''
+    try:
+        data = pd.read_csv(path)
+    except FileNotFoundError as error:
+        print(FileNotFoundError.__name__, error, sep=": ")
+    except pd.errors.EmptyDataError:
+        print("Le fichier est vide.")
+    except pd.errors.ParserError:
+        print("Erreur de parsing. Vérifiez le format du fichier.")
+    except pd.errors.DtypeWarning as e:
+        print(f"Warning de type : {e}")
+    except ValueError as error:
+        print(ValueError.__name__, error, sep=": ")
+    # except Exception as e:
+    #     print(f"Une erreur inattendue s'est produite : {e}")
+        
+    else:
+        print(f"Loading dataset of dimensions {data.shape}")
+        return data
 
 
 if __name__ == "__main__":
-    print(load("life_expectancy_years.csv"))
+    # print(load("life_expectancy_years.csv"))
+    # print(load("empty.csv"))
+    # print(load(123))
+    # print(load("file_not_exist.csv"))
+    print(load("life_expectancy_years.zip"))
