fa1da3accfdf611bad97ea114a3d0cd06ce22844
lollith <adeline.gouet@gmail.com>
2023-11-28 17:17:16 +0100

    en cours

--- 2-DataTable/ex01/aff_life.py
+++ 2-DataTable/ex01/aff_life.py
@@ -6,26 +6,49 @@ import matplotlib.pyplot as plt
 your campus"""
 
 
+# def main():
+#     try:
+#         mydata = load("life_expectancy_years.csv")
+#     except FileNotFoundError as e:
+#         print(FileNotFoundError.__name__, e, sep=": ")
+#     else:
+#         # set index permet d enlever les index sinon passe comme colonnes
+#         try:
+#             mydata_transposed = mydata.set_index("country").T
+#         except AttributeError as er:
+#             print(AttributeError.__name__, er, seo=": ")
+#         else:
+#             mylineplot = sns.lineplot(data=mydata_transposed,
+#                                       x=mydata_transposed.index, y="France")
+#             mylineplot.set_xlabel("Year")
+#             mylineplot.set_ylabel("Life expectancy")
+#             mylineplot.set_title('France Life expectancy Projections')
+#             mylineplot.set_xticks(range(0, 300, 40))
+#             plt.show()
+
+
 def main():
     try:
         mydata = load("life_expectancy_years.csv")
     except FileNotFoundError as e:
         print(FileNotFoundError.__name__, e, sep=": ")
     else:
-        # set index permet d enlever les index sinon passe comme colonnes
         try:
-            mydata_transposed = mydata.set_index("country").T
+            # Utilisez la fonction melt pour obtenir un format long
+            mydata_long = mydata.melt(id_vars=["country"], var_name="Year", value_name="Life Expectancy")
         except AttributeError as er:
-            print(AttributeError.__name__, er, seo=": ")
+            print(AttributeError.__name__, er, sep=": ")
         else:
-            mylineplot = sns.lineplot(data=mydata_transposed,
-                                      x=mydata_transposed.index, y="France")
+            # Filtrez les données pour la France
+            mydata_france = mydata_long[mydata_long["country"] == "France"]
+
+            # Utilisez seaborn pour tracer le graphique
+            mylineplot = sns.lineplot(data=mydata_france, x="Year", y="Life Expectancy")
             mylineplot.set_xlabel("Year")
-            mylineplot.set_ylabel("Life expectancy")
-            mylineplot.set_title('France Life expectancy Projections')
+            mylineplot.set_ylabel("Life Expectancy")
+            mylineplot.set_title('France Life Expectancy Projections')
             mylineplot.set_xticks(range(0, 300, 40))
             plt.show()
-
 
 if __name__ == "__main__":
     main()
--- 2-DataTable/ex02/aff_life.py
+++ 2-DataTable/ex02/aff_life.py
@@ -1,10 +1,22 @@
 import seaborn as sns
 from load_csv import load
 import matplotlib.pyplot as plt
+import pandas as pd
 
 """program that load the good file and displays the country information of
 your campus"""
+def clean_value(x):
+    if not x:
+        return 0
 
+    if isinstance(x, str):
+        if 'M' in x:
+            return int(float(x.replace('M', '')) * 1e6)
+        elif 'k' in x:
+            return int(float(x.replace('k', '')) * 1e3)
+        elif 'B' in x:
+            return int(float(x.replace('B', '')) * 1e12)
+    return int(x)
 
 def main():
     try:
@@ -12,26 +24,29 @@ def main():
     except FileNotFoundError as e:
         print(FileNotFoundError.__name__, e, sep=": ")
     else:
-        # set index permet d enlever les index sinon passe comme colonnes
         try:
+            # set index permet d enlever les index sinon passe comme colonnes
             mydata_transposed = mydata.set_index("country").T
-            print(mydata_transposed)
-            print(mydata_transposed.index)
+            # print(mydata_transposed)
         except AttributeError as er:
             print(AttributeError.__name__, er, sep=": ")
         else:
+            mydata_transposed = mydata_transposed.map(clean_value)
+            print(mydata_transposed)
+            print(mydata_transposed.index)
+            
+            # mylineplot = sns.lineplot(data=mydata_transposed,
+                                    #   x=mydata_transposed.index, y="Belgium")
+            print(mydata_transposed["France"])
             mylineplot = sns.lineplot(data=mydata_transposed,
                                       x=mydata_transposed.index, y="France")
-            # mylineplot = sns.lineplot(data=mydata_transposed,
-            #                           x=mydata_transposed.index, y="Belgium")
-            mylineplot.set_xlabel("Year")
-            mylineplot.set_ylabel("Population")
-            mylineplot.set_title('Population Projections')
-            mylineplot.set_xticks(range(0, 300, 40))
-            mylineplot.set_xlim(-10, 260)
-            mylineplot.set_yticks(range(0, 3, 20000000))
-            
-            plt.show()
+            # mylineplot.set_xlabel("Year")
+            # mylineplot.set_ylabel("Population")
+            # mylineplot.set_title('Population Projections')
+            # mylineplot.set_xticks(range(0, 240 + 1, 40))
+            # mylineplot.set_xlim(0, 250)
+            # plt.legend(labels=["Belgium", "France"])
+            # plt.show()
 
 
 if __name__ == "__main__":
