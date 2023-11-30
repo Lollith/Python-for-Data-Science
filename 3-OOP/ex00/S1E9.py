968bd77ca21d06fda3d7e4cbd3705c7bdda3be1a
lollith <adeline.gouet@gmail.com>
2023-11-30 17:44:40 +0100

    ok

---
+++ 3-OOP/ex00/S1E9.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+class Character(ABC):
+	""" A Class used to create a character, 2 protected parameters"""
+
+  		_name
+ 
+	def __init__():
+     """Initiate the object. Requires .... argument, "...", which is the
+	..."""
+    
+ 
+	@abstractmethod
+	#your code here
+
+
+class Stark(Character):
+	"""An inherited class of Character"""
+	#your code here
+ 
+ 
+
+if __name__ == "__main__":
