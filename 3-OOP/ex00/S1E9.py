5f33db02571149c37a8cb31d43d416b176bfcf8b
lollith <adeline.gouet@gmail.com>
2023-12-04 17:54:47 +0100

    ex00

--- 3-OOP/ex00/S1E9.py
+++ 3-OOP/ex00/S1E9.py
@@ -1,23 +1,45 @@
 from abc import ABC, abstractmethod
 
-class Character(ABC):
-	""" A Class used to create a character, 2 protected parameters"""
 
-  		_name
- 
-	def __init__():
-     """Initiate the object. Requires .... argument, "...", which is the
-	..."""
-    
- 
-	@abstractmethod
-	#your code here
+class Character(ABC):
+    '''A Class used to create a character, 2 protected parameters'''
+    def __init__(self, first_name, is_alive=True):
+        '''Initiate the object. 2 arguments, first_name and is_alive.'''
+        self.__first_name = first_name
+        self.is_alive = is_alive
+
+    @abstractmethod
+    def die(self):
+        '''Method which change health state of the caracter.'''
+        pass
 
 
 class Stark(Character):
-	"""An inherited class of Character"""
-	#your code here
- 
- 
+    '''An inherited class of Character, create a Stark.'''
+    def __init__(self, first_name, is_alive=True):
+        '''Initiate the object. 2 arguments, first_name and is_alive.'''
+        self.__first_name = first_name
+        self.is_alive = is_alive
+
+    def die(self):
+        '''Method which change health state of the caracter.'''
+        self.is_alive = False
+
 
 if __name__ == "__main__":
+    Ned = Stark("Ned")
+    print(Ned.__dict__)
+    print(Ned.is_alive)
+    Ned.die()
+    print(Ned.is_alive)
+    print(Ned.__doc__)
+    print(Ned.__init__.__doc__)
+    print(Ned.die.__doc__)
+    print("---")
+    Lyanna = Stark("Lyanna", False)
+    print(Lyanna.__dict__)
+
+    try:
+        hodor = Character("hodor")
+    except TypeError as e:
+        print(TypeError.__name__, e, sep=": ")
