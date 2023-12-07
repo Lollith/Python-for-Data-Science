5e085eac5d62b7c38c5aec752b1be335e6314368
lollith <adeline.gouet@gmail.com>
2023-12-07 13:36:03 +0100

    fin du  3

--- 3-OOP/ex03/ft_calculator.py
+++ 3-OOP/ex03/ft_calculator.py
@@ -2,16 +2,33 @@ class calculator:
     ''' calculator class ables to do calculations: addition, multiplication,\
         substraction and division of vector with a scalar'''
 
-    def __init__(self):
-        ''' Initializes the calculator'''
-    def __add__(self, object) -> None:
-        ''' '''
-    def __mul__(self, object) -> None:
-        ''' '''
-    def __sub__(self, object) -> None:
-        ''' '''
-    def __truediv__(self, object) -> None:
-        ''' '''
+    def __init__(self, my_object):
+        '''Initializes the calculator.'''
+        self.my_object = my_object
+
+    def __add__(self, object_right) -> None:
+        '''Addition of 2 objects and print a new object.'''
+        self.my_object = ([i + object_right for i in self.my_object])
+        print(self.my_object)
+
+    def __mul__(self, object_right) -> None:
+        '''multiplication of 2 objects and print a new object'''
+        self.my_object = [i * object_right for i in self.my_object]
+        print(self.my_object)
+
+    def __sub__(self, object_right) -> None:
+        '''substraction of 2 objects and print a new object'''
+        self.my_object = [i - object_right for i in self.my_object]
+        print(self.my_object)
+
+    def __truediv__(self, object_right) -> None:
+        '''division of 2 objects ans print a new object'''
+        try:
+            self.my_object = [i / object_right for i in self.my_object]
+        except ZeroDivisionError as e:
+            print(ZeroDivisionError.__name__, e, sep=": ")
+        else:
+            print(self.my_object)
 
 
 if __name__ == "__main__":
@@ -23,4 +40,5 @@ if __name__ == "__main__":
     print("---")
     v3 = calculator([10.0, 15.0, 20.0])
     v3 - 5
-    v3 / 5
+    v3 / 5  # manip de l'instance
+    v3 / 0
---
+++ 3-OOP/ex04/ft_calculator.py
@@ -0,0 +1,41 @@
+class Calculator:
+    '''Calculator class ables to do dotproduct, addition, substraction of 2\
+        vectors '''
+
+    @classmethod
+    def dotproduct(cls, V1: list[float], V2: list[float]) -> None:
+        try:
+            if (len(V1) == len(V2)):
+                print(sum([i * j for i, j in zip(V1, V2)]))
+            else:
+                raise AssertionError("Not same size")
+        except AssertionError as e:
+            print(AssertionError.__name__, e, sep=": ")
+
+    @classmethod
+    def add_vec(cls, V1: list[float], V2: list[float]) -> None:
+        try:
+            if (len(V1) == len(V2)):
+                print([float(i + j) for i, j in zip(V1, V2)])
+            else:
+                raise AssertionError("Not same size")
+        except AssertionError as e:
+            print(AssertionError.__name__, e, sep=": ")
+
+    @classmethod
+    def sous_vec(cls, V1: list[float], V2: list[float]) -> None:
+        try:
+            if (len(V1) == len(V2)):
+                print([float(i - j) for i, j in zip(V1, V2)])
+            else:
+                raise AssertionError("Not same size")
+        except AssertionError as e:
+            print(AssertionError.__name__, e, sep=": ")
+
+
+if __name__ == "__main__":
+    a = [5, 10, 2]
+    b = [2, 4, 3]
+    Calculator.dotproduct(a, b)
+    Calculator.add_vec(a, b)
+    Calculator.sous_vec(a, b)
